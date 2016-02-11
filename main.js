616ec3dddbbee0ebf9513d6a975bcbeea7910eab
Gurjot Singh <Laugh_and_drive@Gurjots-MacBook-Pro.local>
2016-02-10 22:49:24 -0800

    all changes fixed

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var app = angular.module('stockTracker', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', { url: '/', templateUrl: './pages/home.html' })  
-    .state('list', { url: '/list', templateUrl: './pages/list.html', controller: 'listCtrl' })  
-    .state('add', { url: '/add', templateUrl: './pages/add.html', controller: 'addCtrl' })  
+  .state('home', { url: '/', templateUrl: './pages/home.html' })  
+  .state('list', { url: '/list', templateUrl: './pages/list.html', controller: 'listCtrl' })  
+  .state('add', { url: '/add', templateUrl: './pages/add.html', controller: 'addCtrl' })  
   $urlRouterProvider.otherwise('/');
 });
 
@@ -23,27 +23,27 @@ app.service('Stocks', function($http){
   this.addStock = function(symbolAdd){
     var promise = $http.jsonp(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=${symbolAdd}&jsoncallback=JSON_CALLBACK`);
     promise.then(function(res){
-    console.log(names);
+      console.log(names);
 
 
-    if(names.indexOf(res.data.Symbol) > 0){
-      swal("Stock is already in list")
+      if(names.indexOf(res.data.Symbol) > 0){
+        swal("Stock is already in list")
 
-    }
-    else if (res.data.Message){
+      }
+      else if (res.data.Message){
         swal("Stock symbol is non existent, please search");
       }
-    else{
-      thisStock.stocks.push(res);
-      names.push(res.data.Symbol);
+      else{
+        thisStock.stocks.push(res);
+        names.push(res.data.Symbol);
 
-      swal("Your Stock Has Been Added")
-      location.href= '/#/list'
-      console.log(res.data.Symbol);
-      console.log(names.indexOf(res.data.Symbol));
+        swal("Your Stock Has Been Added")
+        location.href= '/#/list'
+        console.log(res.data.Symbol);
+        console.log(names.indexOf(res.data.Symbol));
       }
     });
-    };
+  };
 
   this.lookUp = function(symbolName){
     return $http.jsonp(`http://dev.markitondemand.com/MODApis/Api/v2/Lookup/jsonp?input=${symbolName}&jsoncallback=JSON_CALLBACK`);
@@ -62,11 +62,11 @@ app.controller('listCtrl', function($scope, $state, Stocks){
   $scope.stocks = Stocks.stocks;
 
 
- $scope.deleteName = function(value){
-  var index = this.$index;
+  $scope.deleteName = function(value){
+    var index = this.$index;
   //console.log(index);
   Stocks.deleteFromAray(index)
-  }
+}
 
 
 });
@@ -84,7 +84,7 @@ app.controller('addCtrl', function($scope, $state, Stocks){
     promise.then(function(res){
       $scope.searchResults = res.data;
       $scope.showTable = true;
-      });
+    });
 
   };
 });
